138a2eba1a98b51148e013ca64b0f39c53694a31
uzairzia <uzair.governorsindh@gmail.com>
2024-03-01 12:24:43 +0500

    initial commit

---
+++ 6_Stripping_Names.js
@@ -0,0 +1,4 @@
+var personname = '      \n\t\tUzair Zia\t\n ';
+console.log(personname);
+var without_whitespace = personname.trim();
+console.log(without_whitespace);
---
+++ 6_Stripping_Names.ts
@@ -0,0 +1,6 @@
+let personname: string = '      \n\t\tUzair Zia\t\n ';
+
+console.log(personname);
+
+let without_whitespace: string = personname.trim();
+console.log(without_whitespace);
